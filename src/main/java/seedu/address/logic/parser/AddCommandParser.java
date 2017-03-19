5f438d1f134b888c8def928c7277da53843b2364
DESKTOP-7RLT252\gjlam95 <gj_lam@hotmail.com>
2017-03-20 00:56:21 +0800

    Tidy up checkstyle

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -9,6 +9,7 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.IncorrectCommand;
 
 /**
  * Parses input arguments and creates a new AddCommand object
@@ -18,20 +19,24 @@ public class AddCommandParser {
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
-     * @throws IllegalValueException 
+     * @throws IllegalValueException
      */
-    public Command parse(String args) throws IllegalValueException {
+    public Command parse(String args) {
         ArgumentTokenizer argsTokenizer =
                 new ArgumentTokenizer(PREFIX_PRIORITY, PREFIX_STATUS, PREFIX_NOTE, PREFIX_DEADLINE, PREFIX_TAG);
         argsTokenizer.tokenize(args);
-        return new AddCommand(
-                argsTokenizer.getPreamble().get(),
-                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_PRIORITY), "priority"),
-                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_STATUS), "status"),
-                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_NOTE), "note"),
-                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_DEADLINE), "deadline"),
-                ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))
-            );
+        try {
+            return new AddCommand(
+                    argsTokenizer.getPreamble().get(),
+                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_PRIORITY), "priority"),
+                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_STATUS), "status"),
+                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_NOTE), "note"),
+                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_DEADLINE), "deadline"),
+                    ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))
+                );
+        } catch (IllegalValueException ive) {
+            return new IncorrectCommand(ive.getMessage());
+        }
     }
 
 }
