d718503172c1c490de8820cf98d3b8a20f867fed
DESKTOP-7RLT252\gjlam95 <gj_lam@hotmail.com>
2017-03-20 00:38:50 +0800

    Remove NoSuchElementException

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -1,18 +1,14 @@
 package seedu.address.logic.parser;
 
-import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_DEADLINE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PRIORITY;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_STATUS;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.NoSuchElementException;
-
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.Command;
-import seedu.address.logic.commands.IncorrectCommand;
 
 /**
  * Parses input arguments and creates a new AddCommand object
@@ -22,25 +18,20 @@ public class AddCommandParser {
     /**
      * Parses the given {@code String} of arguments in the context of the AddCommand
      * and returns an AddCommand object for execution.
+     * @throws IllegalValueException 
      */
-    public Command parse(String args) {
+    public Command parse(String args) throws IllegalValueException {
         ArgumentTokenizer argsTokenizer =
                 new ArgumentTokenizer(PREFIX_PRIORITY, PREFIX_STATUS, PREFIX_NOTE, PREFIX_DEADLINE, PREFIX_TAG);
         argsTokenizer.tokenize(args);
-        try {
-            return new AddCommand(
-                    argsTokenizer.getPreamble().get(),
-                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_PRIORITY), "priority"),
-                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_STATUS), "status"),
-                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_NOTE), "note"),
-                    ParserUtil.toValue(argsTokenizer.getValue(PREFIX_DEADLINE), "deadline"),
-                    ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))
+        return new AddCommand(
+                argsTokenizer.getPreamble().get(),
+                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_PRIORITY), "priority"),
+                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_STATUS), "status"),
+                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_NOTE), "note"),
+                ParserUtil.toValue(argsTokenizer.getValue(PREFIX_DEADLINE), "deadline"),
+                ParserUtil.toSet(argsTokenizer.getAllValues(PREFIX_TAG))
             );
-        } catch (NoSuchElementException nsee) {
-            return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
-        } catch (IllegalValueException ive) {
-            return new IncorrectCommand(ive.getMessage());
-        }
     }
 
 }
